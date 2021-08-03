607374eba84f237cf9d7dfb6776a98df70bef4ee
Callum Floume <flouc001@gmail.com>
2021-08-03 12:13:52 +0100

    retab install file

--- install.zsh
+++ install.zsh
@@ -1,43 +1,43 @@
 export DOTFILES_REPO=$HOME/.dotfiles
 
 link_file () {
-	src=$1
-	dst=$2
-	if [[ -f $dst || -h $dst ]] {
-		read -q "ans?Would you like to overwrite $dst? "
-		echo ""
-		if [[ $ans == "y" ]] {
-			ln -fhsv $src $dst
-		}
-	} else {
-		ln -sv $src $dst
-	}
+  src=$1
+  dst=$2
+  if [[ -f $dst || -h $dst ]] {
+    read -q "ans?Would you like to overwrite $dst? "
+    echo ""
+    if [[ $ans == "y" ]] {
+      ln -fhsv $src $dst
+    }
+  } else {
+    ln -sv $src $dst
+  }
 }
 
 echo "Loading files from: $DOTFILES_REPO"
 
 install_dotfiles () {
-	echo "Symlinking config files..."
-	for src in $(find -H "$DOTFILES_REPO" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
-	do
-		dst="$HOME/.$(basename "${src%.*}")"
-		link_file "$src" "$dst"
-	done
+  echo "Symlinking config files..."
+  for src in $(find -H "$DOTFILES_REPO" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  do
+    dst="$HOME/.$(basename "${src%.*}")"
+    link_file "$src" "$dst"
+  done
 }
 
 install_git () {
-	echo "Installing git config..."
-	if [[ ! -f "$HOME/.gitconfig" ]] {
-		creds_file="$DOTFILES_REPO/git/.creds"
-		if [[ -f $creds_file ]] {
-			source $creds_file
+  echo "Installing git config..."
+  if [[ ! -f "$HOME/.gitconfig" ]] {
+    creds_file="$DOTFILES_REPO/git/.creds"
+    if [[ -f $creds_file ]] {
+      source $creds_file
 
-			git config --global user.name "$GH_AUTHOR_NAME"
-			git config --global user.email "$GH_EMAIL"
-			git config --global github.user "$GH_USER"
-			git config --global include.path "$DOTFILES_REPO/git/gitconfig.ini"
-		}
-	}
+      git config --global user.name "$GH_AUTHOR_NAME"
+      git config --global user.email "$GH_EMAIL"
+      git config --global github.user "$GH_USER"
+      git config --global include.path "$DOTFILES_REPO/git/gitconfig.ini"
+    }
+  }
 }
 
 run_installs () {
